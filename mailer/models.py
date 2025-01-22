6e6bf1aa10e07cabe213533a516e8f41f8d86907
mujahid sultan <mujahid.sultan@mlsoft.ai>
2025-01-22 04:11:51 +0000

    template changes

--- mailer/models.py
+++ mailer/models.py
@@ -5,8 +5,13 @@ from django.db import models
 #     name = models.CharField(max_length=255)
 #     email = models.EmailField(null=True, blank=True)
 
+# class Compaign(models,Model):
+#     # compaign_name = models.CharField(prmarykey = 'yes') 
+#     pass
+
+
 class Email(models.Model):
-    email_address = models.EmailField()
+    email_address = models.EmailField() # prmarykey 
     name = models.CharField(max_length=255)
     added_at = models.DateTimeField(auto_now_add=True)
     # subject = models.CharField(max_length=255)
